72615d3bd2414ec17e827835cafb3511a5845ab9
ozi543 <ozi.pvt@gmail.com>
2022-02-08 16:37:37 +0530

    adding code book

--- code book.md
+++ code book.md
@@ -1,1 +1,39 @@
 # gettingAndCleaningData
+The Script, run_analysis is created to download and prepare the data.
+
+## Downloading data
+
+Dataset used for the project can be found on https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+A full description is available at the site where the data was obtained:
+http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
+
+References: Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. A Public Domain Dataset for Human Activity Recognition Using Smartphones. 21th European Symposium on Artificial Neural Networks, Computational Intelligence and Machine Learning, ESANN 2013. Bruges, Belgium 24-26 April 2013.
+
+## Preperation of data for the analysis
+
+### Getting Data into R
++ Features
+  + Features are generated from the accelerometer and gyroscope. 3 axial raw signals are used, tAcc-XYZ and tGyro-XYZ.
+  + Read data as a dataframe from file 'data/features.txt' and assign to variable 'feat' (Line-22:23).
+  + 561 rows, 2 columns
++ Activity labels
+  + Corresponding label for the activity codes.
+  + Read data as a dataframe from file 'data/activity_labels.txt' and assign to variable 'act' (Line-24:25).
+  + 6 rows, 2 columns
++ Test data
+  + Subjects of the test data are read from the file "data/test/subject_test.txt" as a data frame and assigned to variable 'sTest' (line-28:29, 2947 rows, 1 column)
+  + Test data are read from the file "data/test/X_test.txt" as a dataframe and assign to variable 'xTest' (line-30:31, 2947 rows, 1 column).
+  + Activities related to each observation are read from "data/test/y_test.txt" as a dataframe and assign to variable'yTest' (line-32:33, 2947 rows, 1 column).
++ Train Data
+  + Subjects of the train data are read from the file "data/train/subject_train.txt" as a data frame and assigned to variable 'sTrain' (line-36:37, 7352 rows, 1 column)
+  + Train data are read from the file "data/train/X_train.txt" as a dataframe and assign to variable 'xTrain' (line-38:39, 7352 rows, 1 column).
+  + Activities related to each observation are read from "data/train/y_train.txt" as a dataframe and assign to variable 'yTrain' (line-40:41, 7352 rows, 1 column).
+  
+### 
+
+
+
+
+
+
--- run_analysis.R
+++ run_analysis.R
@@ -3,72 +3,83 @@ library(data.table)
 
 # Getting data
 
-if(!file.exists("data/README.txt")){
-  if(!file.exists("data/raw.zip")) {
+if (!file.exists("data/README.txt")) {
+  if (!file.exists("data/raw.zip")) {
     url <-
       'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
     download.file(url, "data/raw.zip", method = "curl")
   }
-  unzip(zipfile = "data/raw.zip",exdir = "data/")
-  file.copy(from = "data/UCI HAR Dataset/.",to = "data/",recursive = T)
-  unlink("data/UCI HAR Dataset/",recursive = T)
+  unzip(zipfile = "data/raw.zip", exdir = "data/")
+  file.copy(from = "data/UCI HAR Dataset/.",
+            to = "data/",
+            recursive = T)
+  unlink("data/UCI HAR Dataset/", recursive = T)
 }
 
 # [data citation]: Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. A Public Domain Dataset for Human Activity Recognition Using Smartphones. 21th European Symposium on Artificial Neural Networks, Computational Intelligence and Machine Learning, ESANN 2013. Bruges, Belgium 24-26 April 2013.
 
 # Reading Data
-feat <- read.table("data/features.txt", col.names = c("n","functions"))
-act <- read.table("data/activity_labels.txt", col.names = c("code", "activity"))
+feat <-
+  read.table("data/features.txt", col.names = c("n", "functions"))
+act <-
+  read.table("data/activity_labels.txt", col.names = c("code", "activity"))
 
 ## Test set
-sTest <- read.table("data/test/subject_test.txt", col.names = "subject")
-xTest <- read.table("data/test/X_test.txt", col.names = feat$functions)
-yTest <- read.table("data/test/y_test.txt", col.names = "activitycode")
+sTest <-
+  read.table("data/test/subject_test.txt", col.names = "subject")
+xTest <-
+  read.table("data/test/X_test.txt", col.names = feat$functions)
+yTest <-
+  read.table("data/test/y_test.txt", col.names = "activitycode")
 
 ## Train set
-sTrain <- read.table("data/train/subject_train.txt", col.names = "subject")
-xTrain <- read.table("data/train/X_train.txt", col.names = feat$functions)
-yTrain <- read.table("data/train/y_train.txt", col.names = "activitycode")
+sTrain <-
+  read.table("data/train/subject_train.txt", col.names = "subject")
+xTrain <-
+  read.table("data/train/X_train.txt", col.names = feat$functions)
+yTrain <-
+  read.table("data/train/y_train.txt", col.names = "activitycode")
 
 # combine train and test data sets
-x<-rbind(xTest,xTrain)
-y<-rbind(yTest,yTrain)
-s<-rbind(sTest,sTrain)
+x <- rbind(xTest, xTrain)
+y <- rbind(yTest, yTrain)
+s <- rbind(sTest, sTrain)
 
 # merging x,y,s
-m_df<-cbind(s,y,x)
+m_df <- cbind(s, y, x)
 
 # Selecting only necessary variables
-df <- m_df %>% select(subject, activitycode, contains("mean"), contains("std"))
+df <-
+  m_df %>% select(subject, activitycode, contains("mean"), contains("std"))
 
 # Replacing activity number by the name of the activity
 df$activitycode <- act[df$activitycode, 2]
 
 # updating the name of the column activitycode to activity
-names(df)[2]<-'activity'
+names(df)[2] <- 'activity'
 
 # print(names(df))
 
 # Replacing short words with descriptive names in variable names
-names(df)<-gsub("Acc", "Accelerometer", names(df))
-names(df)<-gsub("Gyro", "Gyroscope", names(df))
-names(df)<-gsub("Mag", "Magnitude", names(df))
-names(df)<-gsub("^t", "Time", names(df))
-names(df)<-gsub("^f", "Frequency", names(df))
-names(df)<-gsub("tBody", "TimeBody", names(df))
+names(df) <- gsub("Acc", "Accelerometer", names(df))
+names(df) <- gsub("Gyro", "Gyroscope", names(df))
+names(df) <- gsub("Mag", "Magnitude", names(df))
+names(df) <- gsub("^t", "Time", names(df))
+names(df) <- gsub("^f", "Frequency", names(df))
+names(df) <- gsub("tBody", "TimeBody", names(df))
 
-names(df)<-gsub("angle", "Angle", names(df))
-names(df)<-gsub("gravity", "Gravity", names(df))
+names(df) <- gsub("angle", "Angle", names(df))
+names(df) <- gsub("gravity", "Gravity", names(df))
 
-names(df)<-gsub("BodyBody", "Body", names(df))
+names(df) <- gsub("BodyBody", "Body", names(df))
 
-names(df)<-gsub("-mean()", "Mean", names(df), ignore.case = TRUE)
-names(df)<-gsub("-std()", "STD", names(df), ignore.case = TRUE)
-names(df)<-gsub("-freq()", "Frequency", names(df), ignore.case = TRUE)
+names(df) <- gsub("-mean()", "Mean", names(df), ignore.case = TRUE)
+names(df) <- gsub("-std()", "STD", names(df), ignore.case = TRUE)
+names(df) <-
+  gsub("-freq()", "Frequency", names(df), ignore.case = TRUE)
 
 dfF <- df %>%
   group_by(subject, activity) %>%
   summarise_all(mean)
 
 str(dfF)
-
