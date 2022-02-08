3a5fc4f15fa8910e16c59e0d5190840679ee8162
Osanda Viraj <ozi.cse20@gmail.com>
2022-02-08 15:45:20 +0530

    adding code book

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,19 @@
 library(dplyr)
 library(data.table)
 
+# Getting data
+
+if(!file.exists("data/README.txt")){
+  if(!file.exists("data/raw.zip")) {
+    url <-
+      'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
+    download.file(url, "data/raw.zip", method = "curl")
+  }
+  unzip(zipfile = "data/raw.zip",exdir = "data/")
+  file.copy(from = "data/UCI HAR Dataset/.",to = "data/",recursive = T)
+  unlink("data/UCI HAR Dataset/",recursive = T)
+}
+
 #[data citation]: Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. A Public Domain Dataset for Human Activity Recognition Using Smartphones. 21th European Symposium on Artificial Neural Networks, Computational Intelligence and Machine Learning, ESANN 2013. Bruges, Belgium 24-26 April 2013.
 
 # Reading Data
@@ -25,32 +38,37 @@ s<-rbind(sTest,sTrain)
 # merging x,y,s
 m_df<-cbind(s,y,x)
 
-# Selecting necessary variables
-
+# Selecting only necessary variables
 df <- m_df %>% select(subject, activitycode, contains("mean"), contains("std"))
 
-df$activitycode <- activities[df$activitycode, 2]
+# Replacing activity number by the name of the activity
+df$activitycode <- act[df$activitycode, 2]
 
+# updating the name of the column activitycode to activity
 names(df)[2]<-'activity'
 
 # print(names(df))
 
+# Replacing short words with descriptive names in variable names
 names(df)<-gsub("Acc", "Accelerometer", names(df))
 names(df)<-gsub("Gyro", "Gyroscope", names(df))
-names(df)<-gsub("BodyBody", "Body", names(df))
 names(df)<-gsub("Mag", "Magnitude", names(df))
 names(df)<-gsub("^t", "Time", names(df))
 names(df)<-gsub("^f", "Frequency", names(df))
 names(df)<-gsub("tBody", "TimeBody", names(df))
+
+names(df)<-gsub("angle", "Angle", names(df))
+names(df)<-gsub("gravity", "Gravity", names(df))
+
+names(df)<-gsub("BodyBody", "Body", names(df))
+
 names(df)<-gsub("-mean()", "Mean", names(df), ignore.case = TRUE)
 names(df)<-gsub("-std()", "STD", names(df), ignore.case = TRUE)
 names(df)<-gsub("-freq()", "Frequency", names(df), ignore.case = TRUE)
-names(df)<-gsub("angle", "Angle", names(df))
-names(df)<-gsub("gravity", "Gravity", names(df))
 
 dfF <- df %>%
   group_by(subject, activity) %>%
   summarise_all(mean)
 
-
 str(dfF)
+
